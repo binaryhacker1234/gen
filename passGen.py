6b2aa9bed681dddb4ca1cd0d537878bdde256ac8
parsemyarse <parsemyarse@gmail.com>
2017-02-16 13:07:27 -0600

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -2,7 +2,18 @@
 import sys, random
 
 def passGenVars(words, minchars, maxchars):
-  for i in random.randrange(0, random.randrange(0, len(words)):
-    for n in random.randrange(minchars, maxchars):
-        workspace = [n]
+  workspace = []
+  comblist = []
+  for i in random.randrange(minchar, maxchar):
+    variables = random.randrange(1, i)
+    n = 1
+    while n < i:
+      if len(list(words[i])) % i == 0:
+        for each in list(str(len(list(words[i])) / i)):
+          comblist.append(each)
+      else:
+        workspace.append(len(list(words[i])) % i)
+        
+        
+        
         
